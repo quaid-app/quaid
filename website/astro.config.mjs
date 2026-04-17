3b3d6af3e511686cd7df45cd7dcd501ca1ed60db
RocketDog <therocketdog@protonmail.com>
2026-04-17 20:14:00 +0800

    docs: style adjjustments

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -18,6 +18,7 @@ export default defineConfig({
       customCss: ["./src/styles/custom.css"],
       components: {
         Header: "./src/components/Header.astro",
+        PageTitle: "./src/components/PageTitle.astro",
       },
       social: [
         {
